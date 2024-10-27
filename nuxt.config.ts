c70e314b52599c8f01ae4e96c5588f288b4acb32
zhanghongyun <1518753183@qq.com>
2024-10-27 16:15:16 +0800

    fix: 修改图标位置

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
         }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
       ]
     }
   },
