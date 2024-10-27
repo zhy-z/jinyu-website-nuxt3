60180c6300672adbe5571a31768f5022b3c2318e
zhanghongyun <1518753183@qq.com>
2024-10-27 15:32:14 +0800

    fix: 修改图标

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  app:{
-    head:{
-      title:"南通景语网络科技有限公司",
-      meta:[
+  app: {
+    head: {
+      title: "南通景语网络科技有限公司",
+      meta: [
         {
-          name:'description',
-          content:"南通景语网络科技有限公司成立于2017年，是海安市第一家专业的游戏公司。南通市海安优质的企业信息服务提供商，拥有专业的技术和实施团队，主要向客户提供数字运营服务、软件开发、云计算、软件定制等各类信息化产品和服务。"
+          name: 'description',
+          content: "南通景语网络科技有限公司成立于2017年，是海安市第一家专业的游戏公司。南通市海安优质的企业信息服务提供商，拥有专业的技术和实施团队，主要向客户提供数字运营服务、软件开发、云计算、软件定制等各类信息化产品和服务。"
         }
       ],
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }
       ]
     }
   },
